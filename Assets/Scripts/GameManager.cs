40b77c537542e47db117700b7569105eed740a7b
Armour <497052684@qq.com>
2017-01-20 22:07:39 -0800

    added basic game flow

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	enum GAME_STATE {
+		GAME_STATE_READY,
+		GAME_STATE_SHOW_SWORD,
+		GAME_STATE_PLAY,
+		GAME_STATE_DIE,
+	};
+
+	public int gameState;
+	public AudioManager audioManager;
+	public PlayerManager playerManager;
+	public EnemyManager enemyManager;
+	public ViveControllerInput viveControllerInput;
+
+	void Awake() {
+		// Awake
+		gameState = GAME_STATE.GAME_STATE_READY;
+	}
+
+	// Use this for initialization
+	void Start() {
+		audioManager = new AudioManager();
+		playerManager = new PlayerManager();
+		enemyManager = new EnemyManager();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	}
+
+}
---
+++ Assets/Scripts/ViveControllerInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ViveControllerInput : MonoBehaviour {
+
+	private SteamVR_TrackedObject trackedObj;
+
+	private SteamVR_Controller.Device Controller {
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+
+	void Awake() {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	public Vector2 GetViveAxis() {
+		return Controller.GetAxis();
+	}
+
+	public bool GetViveTriggerDown() {
+		return Controller.GetHairTriggerDown();
+	}
+	 
+	public bool GetViveTriggerUp() {
+		return Controller.GetHairTriggerUp();
+	}
+
+	public bool GetViveGripDown() {
+		return Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip);
+	}
+
+	public bool GetViveGripUp() {
+		return Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Update
+	}
+}
