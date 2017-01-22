d085c007b83244002220bed0449fed1e4c610b29
TonyStark <zhizhouj@sfu.ca>
2017-01-21 18:28:55 -0800

    update blade

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
     public GameObject BladePrefab;
+	public GameObject BladePrefab_2;
 
     private GameManager _gameManager;
     private GameObject blade1;
+	private GameObject blade2;
 
     public float bladeLength;
 
@@ -16,6 +18,8 @@ public class PlayerManager : MonoBehaviour {
         _gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
         blade1 = Instantiate (BladePrefab);
         blade1.GetComponent<Blade> ().Initialize (0, bladeLength, 0.2f);
+		blade2 = Instantiate (BladePrefab_2);
+		blade2.GetComponent<Blade> ().Initialize (1, bladeLength, 0.2f);
     }
 
     void Update() {
