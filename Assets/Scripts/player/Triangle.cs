d5f6bbcd9adc10b47f71df707eab21d407e7a4ea
luyor <luyuhelyh@gmail.com>
2017-01-22 03:14:09 -0800

    blade color

--- Assets/Scripts/player/Triangle.cs
+++ Assets/Scripts/player/Triangle.cs
@@ -9,15 +9,17 @@ public class Triangle : MonoBehaviour
     private Vector2[] UV = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };
     private int[] triangles = new int[] { 0, 1, 2 };
     private float blockStayTime;
+    private Color color;
 
     void Start()
     {
         startTime = Time.time;
     }
 
-    public void Initialize(Vector3[] vertices, float blockStayTime)
+    public void Initialize(Vector3[] vertices, float blockStayTime, Color color)
     {
         this.blockStayTime = blockStayTime;
+        this.color = color;
         transform.position = new Vector3(0, 0, 0);
         Mesh mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
@@ -36,7 +38,9 @@ public class Triangle : MonoBehaviour
         }
         Color color = this.gameObject.GetComponent<MeshRenderer>().material.color;
         color.a = 1 - (Time.time - startTime) / blockStayTime;
-        color.g = color.b = 1f - (Time.time - startTime) * 0.3f / blockStayTime;
+        color.r = this.color.r + (1 - this.color.r) * (Time.time - startTime) / blockStayTime;
+        color.g = this.color.g + (1 - this.color.g) * (Time.time - startTime) / blockStayTime;
+        color.b = this.color.b + (1 - this.color.b) * (Time.time - startTime) / blockStayTime;
         this.gameObject.GetComponent<MeshRenderer>().material.color = color;
     }
     
