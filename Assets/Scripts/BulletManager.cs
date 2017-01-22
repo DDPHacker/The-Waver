869178ab2a3fd3a12a67d47dc12165177c6c2ad0
TonyStark <zhizhouj@sfu.ca>
2017-01-21 22:54:34 -0800

    update

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour {
+
+	// Use this for initialization
+	LineRenderer lineRend;
+	public Transform startPos;
+	public Transform endPos;
+
+	void Start () {
+		lineRend = GetComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lineRend.SetPosition (0, startPos.position);
+		lineRend.SetPosition (1, endPos.position);
+	}
+}
